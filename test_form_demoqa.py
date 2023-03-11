23b5e2d7690dd5c24178bf5ee6d2690772b0af77
peresypkin.maQA <peresypkin.ma@sibvaleo.com>
2023-03-11 23:07:36 +0700

    добавил заполнение поля с датой рождения

--- test_form_demoqa.py
+++ test_form_demoqa.py
@@ -1,5 +1,5 @@
 from selene.support.shared import browser
-from selene import be, have
+from selene import be, have, command
 import pytest
 
 
@@ -9,6 +9,7 @@ def test_fill_and_submit_form(open_base_page):
     browser.element('#lastName').type('testName')
     browser.element('#gender-radio-1').type(' ')
     browser.element('#userNumber').type('9232455644')
-    browser.element('#dateOfBirthInput').type('9232455644')
-
+    browser.element('#dateOfBirthInput').click()
+    browser.element("[value='2000']").click()
+    browser.element("[aria-label='Choose Friday, March 10th, 2000']").click()
     pass
