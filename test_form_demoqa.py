5f9491213bd8d72b61701f7686ce38a5092952fc
peresypkin.maQA <peresypkin.ma@sibvaleo.com>
2023-03-11 19:41:52 +0700

    fill userNumber

--- test_form_demoqa.py
+++ test_form_demoqa.py
@@ -8,4 +8,7 @@ def test_fill_and_submit_form(open_base_page):
     browser.element('#firstName').type('testName')
     browser.element('#lastName').type('testName')
     browser.element('#gender-radio-1').type(' ')
+    browser.element('#userNumber').type('9232455644')
+    browser.element('#dateOfBirthInput').type('9232455644')
+
     pass
