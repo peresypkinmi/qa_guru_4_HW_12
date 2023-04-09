9c7c1fd8a431e7d3ec3ab5da8e93cd87abccfaa0
peresypkin.maQA <peresypkin.ma@sibvaleo.com>
2023-04-09 11:19:35 +0700

    create mid-level test_fill_and_submit_form

---
+++ pages/registration_form_page.py
@@ -0,0 +1,94 @@
+import os
+from selene import be, have
+from selene.support.shared import browser
+from selenium.webdriver import Keys
+
+
+class RegistrationForm:
+
+    def open_student_registration_form(self):
+        browser.open('automation-practice-form')
+
+
+    def type_first_name_field(self, value):
+        browser.element('#firstName').type(value)
+
+
+    def type_last_name_field(self, value):
+        browser.element('#lastName').type(value)
+
+
+    def type_email_field(self, value):
+        browser.element('#userEmail').type(value)
+
+
+    def choose_radio_male_gender(self):
+        browser.element('[for=gender-radio-1]').click()
+
+
+    def type_phone_field(self, value):
+        browser.element('#userNumber').type(value)
+
+
+
+    def choose_date_of_birth(self):
+        browser.element('#dateOfBirthInput').click()
+        browser.element(f'[value="2000"]').click()
+        browser.element(f'option[value="2"]').click()
+        browser.element(f'[aria-label="Choose Friday, March 10th, 2000"]').click()
+
+
+    def choose_subjects(self, *subjects):
+        browser.element("#subjectsInput").should(be.blank).click()
+        [browser.element("#subjectsInput").should(be.blank).type(subject).press(Keys.ENTER) for subject in subjects]
+
+
+    def choose_hobbies(self):
+        browser.element("[for=hobbies-checkbox-2]").should(be.clickable).click()
+
+
+    def upload_picture(self):
+        os.chdir(f'{os.getcwd()}/../source')
+        browser.element("#uploadPicture").type(os.getcwd() + '/testPicture.png')
+
+
+    def type_current_address(self, value):
+        browser.element("#currentAddress").type(value)
+
+
+    def select_state(self, value):
+        browser.element("#react-select-3-input").type(value).press(Keys.ENTER)
+
+
+    def select_city(self, value):
+        browser.element("#react-select-4-input").type(value).press(Keys.ENTER)
+
+
+    def submit_form(self):
+        browser.element("#submit").press(Keys.ENTER)
+
+
+    def assert_typed_form(self):
+        browser.element("[class='modal-title h4']").should(have.exact_text('Thanks for submitting the form'))
+        browser.all("table>tbody>tr:nth-of-type(1)").should(have.exact_texts('Student Name Ivan Ivanov'))
+        browser.all("table>tbody>tr:nth-of-type(2)").should(have.exact_texts('Student Email testName@test.kk'))
+        browser.all("table>tbody>tr:nth-of-type(3)").should(have.exact_texts('Gender Male'))
+        browser.all("table>tbody>tr:nth-of-type(4)").should(have.exact_texts('Mobile 9232455644'))
+        browser.all("table>tbody>tr:nth-of-type(5)").should(have.exact_texts('Date of Birth 10 March,2000'))
+        browser.all("table>tbody>tr:nth-of-type(6)").should(have.exact_texts('Subjects Maths, History'))
+        browser.all("table>tbody>tr:nth-of-type(7)").should(have.exact_texts('Hobbies Reading'))
+        browser.all("table>tbody>tr:nth-of-type(8)").should(have.exact_texts('Picture testPicture.png'))
+        browser.all("table>tbody>tr:nth-of-type(9)").should(have.exact_texts('Address testAddress'))
+        browser.all("table>tbody>tr:nth-of-type(10)").should(have.exact_texts('State and City Rajasthan Jaipur'))
+
+
+
+
+
+
+
+
+
+
+
+
--- test/test_form_demoqa.py
+++ test/test_form_demoqa.py
@@ -1,41 +1,25 @@
-from operator import and_
-
-from selene.support.shared import browser
-from selene import be, have
-import os
-from selenium.webdriver.common.keys import Keys
+from pages.registration_form_page import RegistrationForm
 
 
 def test_fill_and_submit_form(set_options_in_browser):
-    browser.open('automation-practice-form')
-    browser.element('.main-header').should(have.exact_text('Practice Form'))
-    browser.element('#firstName').should(be.blank).type('testName')
-    browser.element('#lastName').should(be.blank).type('testLastName')
-    browser.element('#userEmail').should(be.blank).type('testName@test.kk')
-    browser.element('[for=gender-radio-1]').should(be.clickable).click()
-    browser.element('#userNumber').should(be.blank).type('9232455644')
-    browser.element('#dateOfBirthInput').should(be.clickable).click()
-    browser.element("[value='2000']").click()
-    browser.element("[aria-label='Choose Friday, March 10th, 2000']").should(be.clickable).click()
-    browser.element("#subjectsInput").should(be.blank).click()
-    browser.element("#subjectsInput").should(be.blank).type('Maths').press(Keys.ENTER)
-    browser.element("#subjectsInput").should(be.blank).type('History').press(Keys.ENTER)
-    browser.element("[for=hobbies-checkbox-2]").should(be.clickable).click()
-    browser.element("#uploadPicture").type(os.getcwd()+'/testPicture.png')
-    browser.element("#currentAddress").should(be.blank).type('testAddress')
-    browser.element("#react-select-3-input").should(be.blank).type('Rajasthan').press(Keys.ENTER)
-    browser.element("#react-select-4-input").should(be.blank).type('Jaipur').press(Keys.ENTER)
-    browser.element("#submit").should(be.clickable).press(Keys.ENTER)
-    browser.element("[class='modal-title h4']").should(have.exact_text('Thanks for submitting the form'))
-    browser.all("table>tbody>tr:nth-of-type(1)").should(have.exact_texts('Student Name testName testLastName'))
-    browser.all("table>tbody>tr:nth-of-type(2)").should(have.exact_texts('Student Email testName@test.kk'))
-    browser.all("table>tbody>tr:nth-of-type(3)").should(have.exact_texts('Gender Male'))
-    browser.all("table>tbody>tr:nth-of-type(4)").should(have.exact_texts('Mobile 9232455644'))
-    browser.all("table>tbody>tr:nth-of-type(5)").should(have.exact_texts('Date of Birth 10 March,2000'))
-    browser.all("table>tbody>tr:nth-of-type(6)").should(have.exact_texts('Subjects Maths, History'))
-    browser.all("table>tbody>tr:nth-of-type(7)").should(have.exact_texts('Hobbies Reading'))
-    browser.all("table>tbody>tr:nth-of-type(8)").should(have.exact_texts('Picture testPicture.png'))
-    browser.all("table>tbody>tr:nth-of-type(9)").should(have.exact_texts('Address testAddress'))
-    browser.all("table>tbody>tr:nth-of-type(10)").should(have.exact_texts('State and City Rajasthan Jaipur'))
-    browser.element('#closeLargeModal').should(be.clickable).click()
 
+    form = RegistrationForm()
+
+    form.open_student_registration_form()
+
+    form.type_first_name_field('Ivan')
+    form.type_last_name_field('Ivanov')
+    form.type_email_field('testName@test.kk')
+    form.choose_radio_male_gender()
+    form.type_phone_field('9232455644')
+    form.choose_date_of_birth()
+    form.choose_subjects('Maths', 'History')
+    form.choose_hobbies()
+    form.upload_picture()
+    form.type_current_address('testAddress')
+    form.select_state('Rajasthan')
+    form.select_city('Jaipur')
+
+    form.submit_form()
+
+    form.assert_typed_form()
