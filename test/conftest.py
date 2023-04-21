cc4496448deedb706354094893bb5b2f10913168
peresypkin.maQA <peresypkin.ma@sibvaleo.com>
2023-04-21 20:03:02 +0700

    added selenoid configs

--- test/conftest.py
+++ test/conftest.py
@@ -1,10 +1,32 @@
 import pytest
 from selene.support.shared import config
+from selene.support.shared import browser
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+
 
 
 @pytest.fixture
-def set_options_in_browser():
+def set_options_in_browser(get_driver_config):
     config.window_width = 1920
     config.window_height = 1080
     config.base_url = 'https://demoqa.com/'
+    browser.config.driver = get_driver_config
+
+
+@pytest.fixture
+def get_driver_config():
+    option = Options()
+    chrome_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": False
+        }
+    }
+    option.capabilities.update(chrome_capabilities)
+    return webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=option)
 
