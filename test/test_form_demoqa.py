d135abd15bcb3e817b9c01761d08f9bef6f991ee
peresypkin.maQA <peresypkin.ma@sibvaleo.com>
2023-04-22 20:40:29 +0700

    added attaches

--- test/test_form_demoqa.py
+++ test/test_form_demoqa.py
@@ -1,5 +1,8 @@
 from pages.registration_form_page import RegistrationForm
 from data.User import User
+from utils import attach
+from selene.support.shared import browser
+
 
 
 def test_fill_and_submit_form(set_options_in_browser):
@@ -15,3 +18,8 @@ def test_fill_and_submit_form(set_options_in_browser):
     form.submit_form()
 
     form.assert_typed_form()
+
+    attach.add_html(browser)
+    attach.add_logs(browser)
+    attach.add_screenshot(browser)
+    attach.add_video(browser)
